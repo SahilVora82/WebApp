6e8d09d2d82b39f41bb65e807302ef41a61ac746
Sahil Vora <sahilvora@Sahils-MacBook-Air.local>
2024-08-01 22:14:06 -0400

    adding database connection

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'database-4.ct64se8ooimh.us-east-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'CongApp1!',
+    database: 'database-4'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+module.exports = connection;
